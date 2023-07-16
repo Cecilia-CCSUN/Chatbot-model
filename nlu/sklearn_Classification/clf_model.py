0dbde420dd7262d803349dfb74b22067759aef77
Cecilia-CCSUN <110511489+Cecilia-CCSUN@users.noreply.github.com>
2023-07-16 12:53:34 +0800

    Update clf_model.py

--- nlu/sklearn_Classification/clf_model.py
+++ nlu/sklearn_Classification/clf_model.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-'''
-@license: (C) Copyright 2013-2020, Node Supply Chain Manager Corporation Limited.
-@time: 2021/6/4 9:34
-@file: clf_model.py
-@author: baidq
-@Software: PyCharm
-@desc:
-'''
+
 import os
 import pickle
 import numpy as np
