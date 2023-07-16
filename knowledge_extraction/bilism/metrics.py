44c808c18efac263416eae4da9b99b6cf1e52859
Cecilia-CCSUN <110511489+Cecilia-CCSUN@users.noreply.github.com>
2023-07-16 12:54:25 +0800

    Update metrics.py

--- knowledge_extraction/bilism/metrics.py
+++ knowledge_extraction/bilism/metrics.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-'''
-@license: (C) Copyright 2013-2020, Node Supply Chain Manager Corporation Limited.
-@time: 2021/5/7 16:01
-@file: metrics.py
-@author: baidq
-@Software: PyCharm
-@desc:
-'''
+
 
 # copied from `https://github.com/chakki-works/seqeval`
 
