38a1a0b1c7de965044f1efdc8de5f42354fcd84f
Cecilia-CCSUN <110511489+Cecilia-CCSUN@users.noreply.github.com>
2023-07-16 12:50:41 +0800

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-'''
-@license: (C) Copyright 2013-2020, Node Supply Chain Manager Corporation Limited.
-@time: 2021/6/4 9:31
-@file: modules.py
-@author: baidq
-@Software: PyCharm
-@desc:
-'''
+
 
 import json
 import requests
