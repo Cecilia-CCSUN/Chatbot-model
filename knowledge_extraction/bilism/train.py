9ceded3df394fb7e45681c8c0aeb938e89d8337d
Cecilia-CCSUN <110511489+Cecilia-CCSUN@users.noreply.github.com>
2023-07-16 12:54:44 +0800

    Update train.py

--- knowledge_extraction/bilism/train.py
+++ knowledge_extraction/bilism/train.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-'''
-@license: (C) Copyright 2013-2020, Node Supply Chain Manager Corporation Limited.
-@time: 2021/5/7 16:06
-@file: train.py
-@author: baidq
-@Software: PyCharm
-@desc:
-'''
+
 
 import keras
 import pickle
